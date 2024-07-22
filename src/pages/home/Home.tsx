31c3779f49c4f534c6df7daa926152ae4a051677
CodeRAFCE <seshan.io@prismscale.com>
2024-07-22 17:29:44 +0530

    update folder name to lower caps

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,14 @@
-import { guestMocks } from "../../shared/mocks"
-import { HomeContainer } from "./Home.style"
-import { GuestCard } from "./sections/GuestCard"
+import { guestMocks } from '../../shared/mocks';
+import { HomeContainer } from './Home.style';
+import { GuestCard } from './sections/guest-card';
 
-const guest = guestMocks[0]
+const guest = guestMocks[0];
 const Home = () => {
   return (
     <HomeContainer>
-      <GuestCard guest={guest}/>
+      <GuestCard guest={guest} />
     </HomeContainer>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
